0f6a43ed870572525978fcf064f047d46ab1df40
jhh021101 <150987243+jhh021101@users.noreply.github.com>
2023-11-22 02:46:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -249,16 +249,6 @@ window.addEventListener("load", function() {
 
   
 
-// 로그인 탭
-  $(document).ready(function(){
-    $('.login_tab_btn li').click(function(){
-        var value = $(this).find('a').attr('href');
-        $('.login_tab_container .login_content').hide();
-        $('.login_tab_container').find(value).show();
-        $(this).addClass('login_active').siblings().removeClass('login_active')
-        return false;
-    });
-});
   
 
 // fqa 리스트 아코디언
